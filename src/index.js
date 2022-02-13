de058e8d6c25aa3f50debe3fccbaab0d96d871d6
LeoKnox <LeoKnox@users.noreply.github.com>
2022-02-12 22:35:58 -0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  React.createElement("h1", null, "Intro to react"),
+  document.getElementById('root')
+);
