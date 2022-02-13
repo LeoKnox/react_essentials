1924370a007dc38ec2b3b7a941e83aabfa548755
LeoKnox <LeoKnox@users.noreply.github.com>
2022-02-13 10:56:23 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Top() {
+  return (
+    <header>
+      <h1>This is the top of the page</h1>
+    </header>
+  );
+}
+
+function Middle() {
+  return (
+    <section>
+      <p>This is where content goes</p>
+    </section>
+  );
+}
+
+function Bottom() {
+  return (
+    <footer>
+      <p>That's all</p>
+    </footer>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Top</h1>
-        <h2>Middle</h2>
-        <h3>Bottom</h3>
+        <Top />
+        <Middle />
+        <Bottom />
       </header>
     </div>
   );
