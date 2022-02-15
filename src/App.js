2f57199af6a7a487e9468fcbb9a69ac2390bdd30
LeoKnox <LeoKnox@users.noreply.github.com>
2022-02-15 10:03:16 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
   const [what, setWhat] = useState("happy");
-  console.log(what);
+
+  useEffect(() => {
+    console.log(`what ${what}`);
+  });
+
   return (
     <>
       <h1>Hello what are you {what}</h1>
