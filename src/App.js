bea76cf474ff1d280cc0ca5b32d8f4b1267612b3
LeoKnox <LeoKnox@users.noreply.github.com>
2022-02-15 10:17:11 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import './App.css';
 
 function App() {
   const [what, setWhat] = useState("happy");
+  const [when, setWhen] = useState("now");
 
   useEffect(() => {
     console.log(`what ${what}`);
-  });
+  }, [what]);
+
+  useEffect(() => {
+    console.log(`when ${when}`)
+  }, [when]);
 
   return (
     <>
-      <h1>Hello what are you {what}</h1>
+      <h1>Hello what are you {what} {when}</h1>
       <button onClick={() => setWhat("amazed")}>Amazed!</button>
       <button onClick={() => setWhat("excite")}>Excited!</button>
+      <button onClick={() => setWhen("later")}>Later</button>
     </>
   );
 }
