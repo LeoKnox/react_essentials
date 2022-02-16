7b1a2368bb14bd9b45650f36e5f2b74ec1ce73e6
LeoKnox <LeoKnox@users.noreply.github.com>
2022-02-16 10:18:24 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,13 +6,19 @@ function App({grade}) {
 
   useEffect(() => {
     fetch(`http://127.0.0.1:8000/grade/?grade=${grade}`)
-    //fetch(`http://127.0.0.1:8000/grade/?grade=2`)
       .then((response) => response.json())
       .then(setData);
   }, []);
 
   if(data) {
-    return <div>{JSON.stringify(data)}</div>
+    //return <div>{JSON.stringify(data)}</div>
+    return (
+      <div>
+        <h1>{data["kanji"]}</h1>
+        <p>{data.meaning}</p>
+        <p>{data.pronounciation}:{data.reading}</p>
+      </div>
+    );
   }
 
   return (
