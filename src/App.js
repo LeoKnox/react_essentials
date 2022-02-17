7af6f9756c6138b43e66b023ced9239463144845
LeoKnox <LeoKnox@users.noreply.github.com>
2022-02-17 10:44:43 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from "react";
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import { Home, Study, Quiz } from "./pages"
 
 function App() {
 
   return (
     <div>
-      <h1>Intro to React testing</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/study" element={<Study />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <App grade="3" />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
