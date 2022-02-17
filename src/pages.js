ceccb5252ac2ce48c17c19561666d0453065b92d
LeoKnox <LeoKnox@users.noreply.github.com>
2022-02-17 10:23:07 -0800

    Add files via upload

---
+++ src/pages.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export function Home() {
+    return (
+        <div>
+            <h1>[Main Page]</h1>
+        </div>
+    )
+
+export function Study() {
+    return (
+        <div>
+            <h1>[Study Page]</h1>
+        </div>
+    )
+
+export function Quiz() {
+    return (
+        <div>
+            <h1>[Quiz Page]</h1>
+        </div>
+    )
+}
